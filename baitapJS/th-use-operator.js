4d7b2c92657acac9c1aeaa3f51188af1886cae52
DinhPATuan <dinhphamanhtuan@gmail.com>
2022-08-11 22:31:44 +0700

    thực hành sử dụng toán tử

---
+++ baitapJS/th-use-operator.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt ("nhập chiều rộng:")
+inputHeight = prompt ("nhập chiều dài:")
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width*height;
+document.write("The area is: " +area)
